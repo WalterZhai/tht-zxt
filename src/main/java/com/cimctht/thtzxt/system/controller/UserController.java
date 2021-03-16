5c26625472cdbac86445d6503d5e08fd30162272
Walter <695015895@qq.com>
2021-03-16 18:05:08 +0800

    产品升级

--- src/main/java/com/cimctht/thtzxt/system/controller/UserController.java
+++ src/main/java/com/cimctht/thtzxt/system/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.cimctht.thtzxt.system.controller;
 
+import com.cimctht.thtzxt.common.entity.JsonResult;
 import com.cimctht.thtzxt.common.entity.TableEntity;
+import com.cimctht.thtzxt.common.exception.UnimaxException;
 import com.cimctht.thtzxt.system.Impl.UserServiceImpl;
+import com.cimctht.thtzxt.system.entity.User;
 import com.cimctht.thtzxt.system.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author Walter(翟笑天)
@@ -47,6 +53,35 @@ public class UserController {
             table = new TableEntity(e);
         }
         return table;
+    }
+
+
+    @GetMapping(value = "/topage/user/loadUserInfo")
+    public ModelAndView topageUserLoadUserInfo(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        String username = (String) session.getAttribute("username");
+        String url = request.getParameter("url");
+        url = url.substring(0, url.lastIndexOf("."));
+        url = url.replace(".", "/");
+        User user = userRepository.findUserByLoginNameAndIsDelete(username,0);
+        ModelAndView modelAndView= new ModelAndView(url).addObject("user",user);
+        return modelAndView;
+    }
+
+    @PostMapping(value = "/user/editBaseInfo")
+    public JsonResult editBaseInfo(HttpServletRequest request) {
+        String id = request.getParameter("id");
+        String email = request.getParameter("email");
+        String mobile = request.getParameter("mobile");
+        try{
+            User user = userRepository.findUserById(id);
+            user.setEmail(email);
+            user.setMobile(mobile);
+            userRepository.save(user);
+            return new JsonResult("修改成功");
+        }catch (Exception e){
+            return new JsonResult(new UnimaxException("修改失败"));
+        }
     }
 
 
