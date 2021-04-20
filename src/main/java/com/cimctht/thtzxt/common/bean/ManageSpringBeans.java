bae4afef89b4b4f1192009b3e03b41787d00d9f3
Walter <695015895@qq.com>
2021-04-20 14:55:26 +0800

    任务调度，多线程中提取主线程中的bean，ManageSpringBeans

---
+++ src/main/java/com/cimctht/thtzxt/common/bean/ManageSpringBeans.java
@@ -0,0 +1,41 @@
+package com.cimctht.thtzxt.common.bean;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Service;
+
+import java.lang.annotation.Annotation;
+import java.util.Map;
+
+/**
+ * @author Walter(翟笑天)
+ * @Date 2021/4/16
+ */
+@Service
+public class ManageSpringBeans implements ApplicationContextAware {
+    private static ApplicationContext context;
+
+    public static <T> T getBean(final Class<T> requiredType) {
+        return context.getBean(requiredType);
+    }
+
+    public static <T> T getBean(final String beanName) {
+        @SuppressWarnings("unchecked")
+        final T bean = (T) context.getBean(beanName);
+        return bean;
+    }
+
+    public static <T> Map<String, T> getBeans(final Class<T> requiredType) {
+        return context.getBeansOfType(requiredType);
+    }
+
+    public static Map<String, Object> getBeansWithAnnotation(final Class<? extends Annotation> annotationType) {
+        return context.getBeansWithAnnotation(annotationType);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
+}
